30aaeaaceeeeba73a77ac779f7b8930edfd7135e
William Mead <bill@meaddesign.net>
2011-12-31 14:15:37 -0800

    new updated database file with dummy data

--- syllabusgen.sql
+++ syllabusgen.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 11, 2011 at 07:55 PM
+-- Generation Time: Dec 31, 2011 at 02:13 PM
 -- Server version: 5.5.9
 -- PHP Version: 5.3.6
 
@@ -102,17 +102,12 @@ CREATE TABLE `competencies` (
   `type` tinyint(1) NOT NULL,
   `ordr` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=87 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=96 ;
 
 --
 -- Dumping data for table `competencies`
 --
 
-INSERT INTO `competencies` VALUES(64, 1, 0, 'Combine principles of design and interactivity to develop user-centered interface concepts.', 0, 1);
-INSERT INTO `competencies` VALUES(65, 1, 0, 'Understand and apply principles of grid structures to layout navigational systems.', 0, 2);
-INSERT INTO `competencies` VALUES(66, 1, 0, 'Use digital type as an expressive and informational tool of communication. ', 0, 3);
-INSERT INTO `competencies` VALUES(67, 1, 0, 'Use color as an expressive and informational tool of communication.', 0, 4);
-INSERT INTO `competencies` VALUES(68, 1, 0, 'Understand the role of timing in an interactive experience.', 0, 5);
 INSERT INTO `competencies` VALUES(78, 10, 0, 'Understand the differences between client-side, server-side, and server-parsed languages ', 0, 1);
 INSERT INTO `competencies` VALUES(79, 10, 0, 'Understand the basics of server, as opposed to workstation design and software', 0, 2);
 INSERT INTO `competencies` VALUES(80, 10, 0, 'Understand common web server software (e.g., Apache, IIS) configuration the differences between client-side, server-side, and server-parsed languages ', 0, 3);
@@ -122,6 +117,11 @@ INSERT INTO `competencies` VALUES(83, 10, 0, 'Understand how server-parsed langu
 INSERT INTO `competencies` VALUES(84, 10, 0, 'Apply basic PHP to create a functional server-parsed program', 0, 7);
 INSERT INTO `competencies` VALUES(85, 10, 0, 'Understand the basics of web-database links', 0, 8);
 INSERT INTO `competencies` VALUES(86, 10, 0, 'Understand the use of CGI and PHP in e-commerce systems', 0, 9);
+INSERT INTO `competencies` VALUES(91, 1, 0, 'Combine principles of design and interactivity to develop user-centered interface concepts.', 0, 1);
+INSERT INTO `competencies` VALUES(92, 1, 0, 'Use digital type as an expressive and informational tool of communication. ', 0, 2);
+INSERT INTO `competencies` VALUES(93, 1, 0, 'Use color as an expressive and informational tool of communication.', 0, 3);
+INSERT INTO `competencies` VALUES(94, 1, 0, 'Understand the role of timing in an interactive experience.', 0, 4);
+INSERT INTO `competencies` VALUES(95, 1, 0, 'Understand and apply principles of grid structures to layout navigational systems.', 0, 5);
 
 -- --------------------------------------------------------
 
@@ -233,7 +233,7 @@ CREATE TABLE `gradingpolicies` (
   `policy` varchar(255) NOT NULL,
   `ordr` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=65 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=68 ;
 
 --
 -- Dumping data for table `gradingpolicies`
@@ -253,6 +253,7 @@ INSERT INTO `gradingpolicies` VALUES(61, 13, NULL, 0, 'All work must be received
 INSERT INTO `gradingpolicies` VALUES(62, 13, NULL, 0, 'Late work receives a grade of zero.', 5);
 INSERT INTO `gradingpolicies` VALUES(63, 13, NULL, 0, 'On-time projects may be redone with instructor approval.', 6);
 INSERT INTO `gradingpolicies` VALUES(64, 13, NULL, 0, 'ABSOLUTELY NO WORK WILL BE ACCEPTED AFTER THE FINAL CLASS MEETS WEEK 11.', 7);
+INSERT INTO `gradingpolicies` VALUES(67, 11, NULL, 0, 'cheese!', 1);
 
 -- --------------------------------------------------------
 
@@ -266,14 +267,14 @@ CREATE TABLE `prereqs` (
   `prereq` varchar(200) NOT NULL,
   `ordr` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `prereqs`
 --
 
-INSERT INTO `prereqs` VALUES(4, 1, 'Permission of Academic Director/Advisor ', 1);
 INSERT INTO `prereqs` VALUES(6, 10, 'MM2233 Intermediate Web-based Programming', 1);
+INSERT INTO `prereqs` VALUES(8, 1, 'Permission of Academic Director/Advisor ', 1);
 
 -- --------------------------------------------------------
 
@@ -286,14 +287,26 @@ CREATE TABLE `sections` (
   `term_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `order` tinyint(2) NOT NULL,
+  `ordr` tinyint(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `sections`
 --
 
+INSERT INTO `sections` VALUES(2, 11, 'sdfsdfsdfs', '<p>sdfsdfs</p>', 1);
+INSERT INTO `sections` VALUES(3, 11, 'section 2', '<p>some stuff for section 2</p>', 2);
+INSERT INTO `sections` VALUES(4, 12, 'sdfsdfsdfs', '<p>sdfsdfs</p>', 1);
+INSERT INTO `sections` VALUES(5, 12, 'section 2', '<p>some stuff for section 2</p>', 2);
+INSERT INTO `sections` VALUES(14, 13, 'Quarter Credit Hour Definition:', '<p>A quarter credit hour is an amount of work represented in intended learning outcomes and verified by evidence of student achievement that is an institutionally established equivalency that reasonably approximates not less than:</p>\r\n<p>(1) One hour of classroom or direct faculty instruction and a minimum of two hours of out-of-class student work each week for 10-12 weeks, or the equivalent amount of work over a different amount of time; or</p>\r\n<p>(2) At least an equivalent amount of work as required in paragraph (1) of this definition for other academic activities as established by the institution including laboratory work, internships, practica, studio work, and other academic work leading to the award of credit hours.</p>', 1);
+INSERT INTO `sections` VALUES(15, 13, 'Classroom Policy:', '<ul>\r\n<li>No food allowed in class or lab at any time. Drinks in recloseable bottles allowed in classroom.</li>\r\n<li>Edible items brought to class or lab must be thrown out.</li>\r\n<li>If student elects to eat/drink outside class or lab door, missed time is recorded as absent.</li>\r\n<li>Attendance is taken hourly. Tardiness or absence is recorded in 15-minute increments.</li>\r\n<li>Break times are scheduled by the instructor at appropriate intervals.</li>\r\n<li>No private software is to be brought to lab or loaded onto school computers.</li>\r\n<li>No software games are allowed in lab (unless in course curriculum).</li>\r\n<li>Headphones are required if listening to music during lab. No headphones are allowed in lecture.</li>\r\n<li>Any student who has special needs that may affect his or her performance in this class is asked to identify his/her needs to the instructor in private by the end of the first day of class. Any resulting class performance problems that may arise for those who do not identify their needs will not receive any special grading considerations.</li>\r\n<li>It is AI-Sacramento policy that cell phones may NOT be used in the classroom. If you have an emergency that requires you to take a call during class, you MUST inform the instructor before class begins, and step outside the room to take the call or text message.</li>\r\n</ul>', 2);
+INSERT INTO `sections` VALUES(16, 13, 'School-wide Attendance Policy:', '<p>Students who do not attend any classes for fourteen (14) consecutive calendar days and fail to notify the Academic Affairs Department will be withdrawn from school.&nbsp; In addition, the student may be involuntarily withdrawn at the discretion of the Academic Director, and with the approval of the Dean of Academic Affairs, at any time.</p>', 3);
+INSERT INTO `sections` VALUES(17, 13, 'Withdraw from a Course:', '<p>In order to withdraw from a course (that is, receive a grade of &ldquo;W&rdquo;), a student must meet with his or her Academic Director before noon on the Friday of week 9.</p>', 4);
+INSERT INTO `sections` VALUES(18, 13, 'Academic Dishonesty:', '<p>Students are expected to maintain the highest standards of academic honesty while pursuing their studies at The Art Institutes. Academic dishonesty includes but is not limited to: plagiarism and cheating; misuse of academic resources or facilities; and misuse of computer software, data, equipment or networks.</p>\r\n<p>Plagiarism is the use (copying) of another person&rsquo;s ideas, words, visual images or audio samples, presented in a manner that makes the work appear to be the student&rsquo;s original creation. All work that is not the student&rsquo;s original creation, or any idea or fact that is not &ldquo;common knowledge,&rdquo; must be documented to avoid even accidental infractions of the conduct code.</p>\r\n<p>Cheating is to gain unfair advantage on a grade by deception, fraud, or breaking the rules set forth by the instructor of the class. Cheating may include but is not limited to: copying the work of others; using notes or other materials when unauthorized; communicating to others during an exam; and any other unfair advantage as determined by the instructor.</p>\r\n<p>Students accused of academic dishonesty will be brought before a Student Conduct Committee. If the committee determines that there has been a violation of the Academic Dishonesty policy, the student will automatically fail the class and, depending on the severity of the infraction, may face further disciplinary action up to and including suspension from classes or expulsion from school.</p>', 5);
+INSERT INTO `sections` VALUES(19, 13, 'Disability Policy Statement:', '<p>It is our policy not to discriminate against qualified students with documented disabilities in our educational programs, activities, or services. If you have a disability-related need for adjustments or other accommodations in this class see Steven Franklin, Director of Student Affairs located on the 2<sup>nd</sup> &nbsp;floor or e-mail him at sfranklin@aii.edu. You must inform your instructors and the Academic Affairs Office before the end of week one of classes and preferably before the class start.</p>', 6);
+INSERT INTO `sections` VALUES(20, 13, 'Student Assistance Program:', '<p>The college provides confidential short-term counseling, crisis intervention, and community referral services through the AllOne Health Student Assistance Program (SAP) for a wide range of concerns, including relationship issues, family problems, loneliness, depression, and alcohol or drug abuse. Services are available 24 hours a day, 7 days a week, at 1.888-617-3362. The Student Affairs office also offers programs on mental health-related topics each quarter. If you have any questions regarding counseling services, please contact the Student Affairs office.</p>', 7);
+INSERT INTO `sections` VALUES(21, 13, 'Library Operation Hours:', '<p>The library is open from 8 AM to 8 PM Monday &ndash; Thursday, 8 AM to 5 PM on Friday and 9 AM to 2 PM on Saturday. The library is closed on Sunday.&nbsp; Computers are available during these hours for students to work on classroom projects.</p>', 8);
 
 -- --------------------------------------------------------
 
