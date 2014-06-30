a65ff7189a34846144d406cea039e83bb5c26d07
William Mead <bill@meaddesign.net>
2014-06-30 18:39:26 -0400

    updated lessonplans.php to include more non-sensicalcontent

--- lessonplans.php
+++ lessonplans.php
@@ -1,13 +1,66 @@
-<!doctype html>
-<html>
+<?php require_once('includes/functions.php'); ?>
+<?php //require_once('includes/functions-execsum.php'); ?>
+<?php session_handler(); ?>
+<?php $link = db_connect(); ?>
+<?php auth_check($link); ?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
 <head>
-<meta charset="UTF-8">
-<title>Lesson Planning</title>
+<meta charset="utf-8">
+
+<meta name="description" content="Art Institute Syllabus Generator">
+<meta name="author" content="William Mead">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Syllabus Generator Lesson Plans</title>
+
+<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,200italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="stylesheets/base.css" type="text/css">
+<link rel="stylesheet" href="stylesheets/layout.css" type="text/css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+
+<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="js/tinymce/jquery.tinymce.min.js"></script>
+
+
 </head>
 
 <body>
 
-	<h1>Lesson Planning, Coming Soon...</h1>
+<div id="page">
+
+	<header id="mainheader">
+    	<div>
+        	<h1>Syllabus<br>Generator</h1>
+        </div>
+	<img src="images/aiLogo.png" alt="logo">
+    </header>
+	<?php include('includes/navigation.php'); ?>
+
+	<section class="whole">
+    
+    <div style="text-align:center">
+    <h3>The lesson plan feature is not ready yet, so for now, enjoy watching Maru fit in a box...</h3>
+    
+    <iframe width="420" height="315" src="//www.youtube.com/embed/rTF2lWHI2IY" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+    </section> 
+
+
 
 </body>
+
 </html>
+
+<?php db_disconnect($link); ?>
